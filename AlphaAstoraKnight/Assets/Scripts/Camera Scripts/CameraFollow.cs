8866ae0d3f6d973083b4182938e101fe0833eb8f
Saul Augusto Gasca Farrera <sgfarrera115@hotmail.com>
2021-05-17 17:13:54 -0500

    Cameta Follow Script and Mouse Cursor Script Added

---
+++ AlphaAstoraKnight/Assets/Scripts/Camera Scripts/CameraFollow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Header("Camera Follow")]
+    public float followHeight = 8.0f;
+    public float followDistance = 6.0f;
+    public Transform player;
+    public float targetHeight;
+    public float currentHeight;
+    public float currentRotation;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetHeight = player.position.y + followHeight;
+
+        currentRotation = transform.eulerAngles.y;
+
+        currentHeight = Mathf.Lerp(transform.position.y,targetHeight, 0.9f * Time.deltaTime);
+
+        Quaternion euler = Quaternion.Euler(0.0f,currentRotation,0.0f);
+
+        Vector3 targetPosition = player.position - (euler * Vector3.forward) * followDistance;
+
+        targetPosition.y = currentHeight;
+
+        transform.position = targetPosition;
+
+        transform.LookAt(player);
+    }
+
+    
+}
---
+++ AlphaAstoraKnight/Assets/Scripts/Camera Scripts/MouseScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour
+{
+    [Header("Own Mouse Cursor")]
+    public Texture2D cursorText;
+    public GameObject mousePoint;
+    private CursorMode cursorMode = CursorMode.ForceSoftware;
+    private Vector2 hotSpot = Vector2.zero;
+     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.SetCursor(cursorText,hotSpot,cursorMode);
+    }
+}
